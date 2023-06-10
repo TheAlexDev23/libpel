a89e3de5887fe016fa1c2ffcc5c18aa1fdf9bec6
TheAlexDev23 <thealexdev23@gmail.com>
2023-06-10 15:25:40 +0200

    Add error handling for text rendering

--- src/shapes/text.c
+++ src/shapes/text.c
@@ -3,11 +3,19 @@
 #include "helper/ft-easy.h"
 
 #include "pel.h"
+#include "state.h"
 
 int pel_write(pel_color_t brush_color, char* text, pel_cord_t cords)
 {
+    CHECK
+
     pel_bitmap_t bm;
-    _tt_easy_get_bm("OpenSans-Regular.ttf", 'I', &bm);
+    if (_tt_easy_get_bm("OpenSans-Regular.ttf", 'I', &bm))
+    {
+        _pel_get_cur_handle()->_err = PEL_ERR_FT_EASY;
+        return -1;
+    }
+
     for (int j = 0; j < bm.height; j++)
     {
         for (int i = 0; i < bm.width; i++)
