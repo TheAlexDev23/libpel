b22fca07b563c4c1c7e02c089435b31abb1ffce5
TheAlexDev23 <thealexdev23@gmail.com>
2023-06-18 12:51:20 +0200

    Remove line gaps that appeared on certain slopes

--- src/shapes/line.c
+++ src/shapes/line.c
@@ -46,8 +46,6 @@ int pel_draw_line(pel_color_t brush_color, pel_cord_t start, pel_cord_t end)
         end._y >= handle->_height)
         return -1;
 
-    _set_color(brush_color);
-
     if (start._x > end._x) {
         pel_cord_t temp = start;
         start = end;
@@ -60,35 +58,64 @@ int pel_draw_line(pel_color_t brush_color, pel_cord_t start, pel_cord_t end)
         end = temp;
     }
 
+    _set_color(brush_color);
+
     int dx, dy;
     get_line_dxdy(start, end, &dx, &dy);
-
-    float m, b;
-    line_eq(start, end, &m, &b);
 
     int ix = dx < 0 ? -1 : 1;
     int iy = dy < 0 ? -1 : 1;
 
+    float m, b;
+    line_eq(start, end, &m, &b);
+
     if (dx > iy * dy) {
+        int ly = m * start._x + b;
         for (int x = start._x; x <= end._x; x += ix) {
             float y = m*(float)x + b;
 
-            int rx = x, ry = (int)round(y);
+            int rx = x, ry = y;
 
             get_xy_rel_img_center(rx, ry, handle);
             _px_set_def_color(rx, ry);
+
+            int lydiff = abs(ly - (int)y);
+
+            if (lydiff > 1) {
+                for (int i = (ly - 1) * iy; i < iy * y; i++) {
+                    int rx = x, ry = i * iy;
+                    get_xy_rel_img_center(rx, ry, handle);
+                    _px_set_def_color(rx, ry);
+                }
+            }
+
+            ly = y;
         }
     }
     else {
+
+        int lx = (start._y - b)/m;
         for (int y = start._y; y <= end._y; y += iy) {
             // y = mx + b
             // x = (y - b) / m
             float x = ((float)y - b) / m;
 
-            int rx = (int)round(x), ry = y;
+            int rx = x, ry = y;
 
             get_xy_rel_img_center(rx, ry, handle);
             _px_set_def_color(rx, ry);
+
+            int lxdiff = abs(lx - (int)x);
+
+            if (lxdiff > 1) {
+                for (int i = (lx - 1) * ix; i < ix * x; i++) {
+                    int rx = i * ix, ry = y;
+                    get_xy_rel_img_center(rx, ry, handle);
+                    _px_set_def_color(rx, ry);
+                }
+            }
+
+            lx = x;
         }
     }
 
